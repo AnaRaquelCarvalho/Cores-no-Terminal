be3f3f5efef07dfab87cd760be2dee018c5d3e94
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-08-24 20:53:00 -0300

    Create Cores1.py

---
+++ Cores1.py
@@ -0,0 +1,17 @@
+print('\033[1:34m========== Operadores Aritméticos ==========')
+
+Adição = 5 + 3
+print(Adição)
+# = 8
+
+Subtração = 5 - 3
+print(Subtração)
+# = 2
+
+Multiplicação = 5 * 3
+print(Multiplicação)
+# = 15
+
+Divisão = 5 / 3
+print(Divisão)
+# = 1.666666666666667
